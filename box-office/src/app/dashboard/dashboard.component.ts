2165c105f7696c4f746a433058f90afbe4ac2c1c
Abhinav Pandey <pandeyabhinav2608@gmail.com>
2022-10-13 00:26:18 +0530

    completed the tasks

---
+++ box-office/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { MOVIES } from '../models/movies';
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  head: string = "Trending Movies";
+  movies = MOVIES;
+
+  searchText:string = "";
+
+  search(){
+    if(this.searchText=== "" || !this.searchText){
+      this.movies = MOVIES;
+    }
+    else{
+      this.movies = MOVIES
+      this.movies = this.movies.filter(movie => movie.movieTitle?.startsWith(this.searchText.toLowerCase()));
+    }
+  }
+
+  reset(){
+    this.searchText=""
+    this.movies=MOVIES
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  
+}
